101f10f839f544276f10fc1b70c05e7f461f1bd3
wunjunjia <2562547453@qq.com>
2020-02-14 12:47:19 +0800

    .

--- routes/advertisement.js
+++ routes/advertisement.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const Advertisement = require('../model/Advertisement');
+const axios = require('axios');
 const advertisementController = require('../controller/AdvertisementController');
 
 const router = express.Router();
 
+// router.get('/list', (req, res) => {
+//   advertisementController.list(req, res);
+// });
+
 router.get('/list', (req, res) => {
-  advertisementController.list(req, res);
+  axios.get('http://localhost:8080/green_travel/api/getAllAdvertisement.action')
+    .then((result) => {
+      const { code, data } = result.data;
+      res.json({
+        code,
+        data: data.map(item => new Advertisement({
+          id: item.ad_id,
+          path: item.ad_path,
+          outside_link: item.ad_link,
+          create_time: item.date,
+          status: item.status,
+          exist: item.exist,
+        })),
+      });
+    }).catch((e) => {
+      console.log(e);
+      res.json({ code: 1 });
+    });
 });
 
 module.exports = router;
